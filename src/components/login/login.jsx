18bf28f6ea637940fcdcfd88c09efb741d7fbb68
bluechiptony <tony5egwu@gmail.com>
2020-05-09 10:52:55 +0100

    docker build changed to add nginx config, jenkinsfile pipeline steps for job added

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useHistory, Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import "./login.scss";
 
 export const Login = () => {
   const { register, handleSubmit, errors } = useForm();
   const history = useHistory();
   const navigate = () => {
+    console.log("History");
+
     history.push("/dashboard");
   };
 
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-lg-7 col-xs-12">
+        <div className="col-lg-7 col-sm-7 col-xs-12">
           <div className="full-page-height centered-flex">
             <div className="login-box center-text">
               <div className="app-form-row">
@@ -33,7 +36,9 @@ export const Login = () => {
                 </span>
               </div>
               <div className="app-form-row">
-                <button className="app-button full-width text-white bg-dark-green uppercase">Login</button>
+                <button className="app-button full-width text-white bg-dark-green uppercase" onClick={navigate}>
+                  Login
+                </button>
               </div>
               <div className="app-form-row">
                 <span className="subtitle">
@@ -47,7 +52,7 @@ export const Login = () => {
             </div>
           </div>
         </div>
-        <div className="col-lg-5 col-xs-12 full-page-height login-img-bg"></div>
+        <div className="col-lg-5 col-sm-5 col-xs-12 full-page-height login-img-bg"></div>
       </div>
     </div>
   );
@@ -87,25 +92,27 @@ export const SignUp = () => {
 
 export const ForgotPassword = () => {
   return (
-    <div className="full-page-height centered-flex">
-      <div className="login-box center-text">
-        <div className="app-form-row">
-          <span className="login-header">Please enter your email address</span>
-        </div>
-        <div className="app-form-row">
-          <input type="text" placeholder="Enter your email address" className="login-input full-width" />
-        </div>
+    <div className="full-page-height centered-flex bg-forgot-password">
+      <div className="container-forgot">
+        <div className="login-box center-text">
+          <div className="app-form-row">
+            <span className="login-header">Please enter your email address</span>
+          </div>
+          <div className="app-form-row">
+            <input type="text" placeholder="Enter your email address" className="login-input full-width" />
+          </div>
 
-        <div className="app-form-row">
-          <button className="app-button full-width text-white bg-dark-green uppercase">Recover account</button>
-        </div>
-        <div className="app-form-row">
-          <span className="subtitle">
-            Don't need this now?{" "}
-            <Link to="/">
-              <span className="bold">Click here</span>
-            </Link>{" "}
-          </span>
+          <div className="app-form-row">
+            <button className="app-button full-width text-white bg-dark-green uppercase">Recover account</button>
+          </div>
+          <div className="app-form-row">
+            <span className="subtitle">
+              Don't need this now?{" "}
+              <Link to="/">
+                <span className="bold">Click here</span>
+              </Link>{" "}
+            </span>
+          </div>
         </div>
       </div>
     </div>
